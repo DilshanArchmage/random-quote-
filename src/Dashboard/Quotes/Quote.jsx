7b0bbd11d95f4bd002ca018fc171c24584b94edf
Dilshan Dissanayake <dilshan@archmage.lk>
2022-05-21 15:38:36 +0530

    ddd

--- src/Dashboard/Quotes/Quote.jsx
+++ src/Dashboard/Quotes/Quote.jsx
@@ -8,7 +8,7 @@ import axios from 'axios'
 export default class Quote extends Component {
     state={
         quote:'The purpose of our lives is to be happy',
-        author:' Dalai Lamadd',
+        author:' Dalai Lddamadd',
         quotesData:[],
         color:'rgb(243,156,18)',
 
